6184f7a8df4714df163195f58b8a0430a117539d
zhac221 <zhac221@live.rhul.ac.uk>
2023-02-22 05:01:49 +0000

    Added back while not rospy is shutdown method and cleaned up code

--- node/move_forward_node.py
+++ node/move_forward_node.py
@@ -1,31 +1,22 @@
+#!/usr/bin/env python
+
 import rospy
 from ackermann_msgs.msg import AckermannDriveStamped
 
-class MoveForward:
-    def __init__(self):
-
-        self.nh = rospy.init_node("move_forward_node")
-
-        self.drive_pub = rospy.Publisher("/f1tenth/ackermann_cmd", AckermannDriveStamped, queue_size=10)
-
-        self.rate = rospy.Rate(10)
-
     
-    def run(self):
-
+def run():
         while not rospy.is_shutdown():
-            drive_cmd = AckermannDriveStamped()
-            drive_cmd.header.stamp = rospy.Time.now()
-            drive_cmd.header.frame_id = "base_link"
-            drive_cmd.drive.speed = 5
-            drive_cmd.drive.steering_angle = 0.0
-            drive_cmd.drive.acceleration = 5
-            self.drive_pub.publish(drive_cmd)
-            self.rate.sleep()
+                drive_cmd = AckermannDriveStamped()
+                drive_cmd.drive.speed = 5
+                drive_pub.publish(drive_cmd)
+            
 
 
 if __name__ == "__main__":
-
-    move_forward = MoveForward()
-
-    move_forward.run()
+        try:
+                rospy.init_node("move_forward_node")
+                drive_pub = rospy.Publisher("/f1tenth/ackermann_cmd", AckermannDriveStamped, queue_size=10)
+                rate = rospy.Rate(500)
+                run()
+        except rospy.ROSInterruptException:
+                pass
