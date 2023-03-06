c90d91cb40fa41cb5054e41b3fd4c0dff500f2f6
zhac221 <zhac221@live.rhul.ac.uk>
2023-03-06 16:43:11 +0000

    Modified code using AckermannDriveStamped instead. Code now works.

--- node/CollisionAvoidance.py
+++ node/CollisionAvoidance.py
@@ -1,33 +1,69 @@
 import rospy
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Float32
+from ackermann_msgs.msg import AckermannDriveStamped
+
+
+# def laser_scan_callback(msg):
+
+#         min_range = min(msg.ranges)
+
+#         if min_range<0.5:
+
+#             drive_cmd = AckermannDriveStamped()
+#             drive_cmd.drive.speed = 0
+#             drive_pub.publish(drive_cmd)
+
+#         else:
+#             drive_cmd = AckermannDriveStamped()
+#             drive_cmd.drive.speed = 2
+#             drive_pub.publish(drive_cmd)
+
+
+    
+
+
+# if __name__ == "__main__":
+#     try:
+#         rospy.init_node("collision_avoidance_node")
+#         rospy.Subscriber("/scan", LaserScan, laser_scan_callback)
+#         drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=10)
+#         rospy.spin()
+#     except rospy.ROSInterruptException:
+#         pass
+
+
+
+
 
 class CollisionAvoidance:
     def __init__(self):
 
         self.nh = rospy.init_node("collision_avoidance_node")
-
         rospy.Subscriber("/scan", LaserScan, self.laser_scan_callback)
+        self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=10)
 
-        self.velocity_pub = rospy.Publisher("/f1tenth/velocity", Float32, queue_size=1000)
+
 
     def laser_scan_callback(self, msg):
+        
 
         min_range = min(msg.ranges)
 
         if min_range < 0.5:
-
-            self.velocity_pub.publish(Float32(0))
+            drive_cmd = AckermannDriveStamped()
+            drive_cmd.drive.speed = 0
+            self.drive_pub.publish(drive_cmd)
 
         else:
-
-            self.velocity_pub.publish(Float32(0.5))
+            drive_cmd = AckermannDriveStamped()
+            drive_cmd.drive.speed = 2
+            self.drive_pub.publish(drive_cmd)
 
 
 if __name__ == "__main__":
 
     collision_avoidance = CollisionAvoidance()
-
     rospy.spin()
+
 
     
